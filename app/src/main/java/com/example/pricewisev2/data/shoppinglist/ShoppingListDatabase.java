eafe89fddb087be0cf18dfab13bdd64c735dac98
jgaurdo03 <jguardoruiz@gmail.com>
2025-04-02 13:47:54 +1100

    Database,responsive and authentication

--- app/src/main/java/com/example/pricewisev2/data/shoppinglist/ShoppingListDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.pricewisev2.data.shoppinglist;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {ShoppingListEntity.class},version = 1)
-public abstract class ShoppingListDatabase extends RoomDatabase {
-    public abstract ShoppingListDao shoppingListDao();
-}
